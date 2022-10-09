234c1f929a6f93befebe32a413a2a876ddceebf4
operezol <oriolperezolivares@gmail.com>
2022-10-09 21:28:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,28 @@ function App() {
   const [keyToPress, setKeyToPress] = useState<string>(keymap[keymapIndex]);
   const [keyStatus, setKeyStatus] = useState<string>("standby");
 
-  useEffect(() => {
-    const ignoredKeys = ["Dead", "NumLock", "End", "Clear", "PageUp", "PageDown", "Home", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Pause", "Insert", "Escape", "Delete", "Backspace", "Alt", "Tab", "CapsLock", "Shift", "Control", "Meta", "AltGraph", "ContextMenu", "Enter", "ArrowLeft", "ArrowDown", "ArrowRight", "ArrowUp"];
+  const ignoredKeys = ["Dead", "NumLock", "End", "Clear", "PageUp", "PageDown", "Home", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Pause", "Insert", "Escape", "Delete", "Backspace", "Alt", "Tab", "CapsLock", "Shift", "Control", "Meta", "AltGraph", "ContextMenu", "Enter", "ArrowLeft", "ArrowDown", "ArrowRight", "ArrowUp"];
 
-    function pressedHandler(event: KeyboardEvent) {
-      if (!ignoredKeys.includes(event.key)) {
-        setKeyPressed(event.key);
-        if (keyPressed === keyToPress) {
-          setKeymapIndex(keymapIndex + 1);
-          setKeyToPress(keymap[keymapIndex]);
-          setKeyStatus("correct");
+  function pressedHandler(event: KeyboardEvent) {
+    if (!ignoredKeys.includes(event.key)) {
+      setKeyPressed(event.key);
+      if (event.key === keyToPress) {
+        if (keymapIndex + 1 < keymap.length) {
+          setKeymapIndex((keymapIndex) => keymapIndex + 1);
+          setKeyToPress(keymap[keymapIndex + 1]);
         } else {
-          setKeyStatus("wrong");
+          setKeymapIndex(0);
+          setKeyToPress(keymap[0]);
         }
+        setKeyStatus("correct");
+      } else {
+        setKeyStatus("wrong");
       }
     }
+  }
 
+  useEffect(() => {
     window.addEventListener("keydown", pressedHandler);
-
     return () => {
       window.removeEventListener("keydown", pressedHandler);
     };
